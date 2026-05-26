55ace4cc65f51867c285f87fc23f5afcfabe4b23
FerreraYera <martin.ferrera@usm.cl>
2026-05-26 19:50:10 -0400

    arreglo MVVM

--- CalculadoraPropinas/obj/Debug/net10.0-android/android/src/mono/MonoPackageManager_Resources.java
+++ CalculadoraPropinas/obj/Debug/net10.0-android/android/src/mono/MonoPackageManager_Resources.java
@@ -3,6 +3,7 @@ public class MonoPackageManager_Resources {
 	public static String[] Assemblies = new String[]{
 		/* We need to ensure that "CalculadoraPropinas.dll" comes first in this list. */
 		"CalculadoraPropinas.dll",
+		"CommunityToolkit.Mvvm.dll",
 		"GoogleGson.dll",
 		"Microsoft.Extensions.Configuration.dll",
 		"Microsoft.Extensions.Configuration.Abstractions.dll",
@@ -147,10 +148,6 @@ public class MonoPackageManager_Resources {
 		"Microsoft.Maui.Controls.resources.dll",
 		"Microsoft.Maui.Controls.resources.dll",
 		"Microsoft.Maui.Controls.resources.dll",
-		"Microsoft.Maui.Controls.HotReload.Forms.dll",
-		"Microsoft.VisualStudio.DesignTools.MobileTapContracts.dll",
-		"Microsoft.VisualStudio.DesignTools.TapContract.dll",
-		"Microsoft.VisualStudio.DesignTools.XamlTapContract.dll",
 		"_Microsoft.Android.Resource.Designer.dll",
 	};
 	public static String[] Dependencies = new String[]{
--- CalculadoraPropinas/obj/Debug/net10.0-ios/iossimulator-x64/CalculadoraPropinas.AssemblyInfo.cs
+++ CalculadoraPropinas/obj/Debug/net10.0-ios/iossimulator-x64/CalculadoraPropinas.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalculadoraPropinas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+f9e78f5dc266400d5acb48299b5041682871e7c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalculadoraPropinas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalculadoraPropinas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
