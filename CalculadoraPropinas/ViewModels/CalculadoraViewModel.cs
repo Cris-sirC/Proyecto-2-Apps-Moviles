ad1b2f9916acd31b6dbe609ac31fd9b2c78aa33f
FerreraYera <martin.ferrera@usm.cl>
2026-05-27 14:54:20 -0400

    a

--- CalculadoraPropinas/ViewModels/CalculadoraViewModel.cs
+++ CalculadoraPropinas/ViewModels/CalculadoraViewModel.cs
@@ -3,12 +3,8 @@ using CommunityToolkit.Mvvm.Input;
 
 namespace CalculadoraPropinas.ViewModels;
 
-// 1. Heredamos de ObservableObject y la clase DEBE ser "partial"
 public partial class CalculadoraViewModel : ObservableObject
 {
-    // 2. Usamos [ObservableProperty] en variables privadas (minúsculas). 
-    // El Toolkit automáticamente creará las propiedades públicas (Mayúsculas) por detrás.
-    
     [ObservableProperty]
     private decimal _totalConsumo;
 
@@ -32,13 +28,10 @@ public partial class CalculadoraViewModel : ObservableObject
         CalcularValores();
     }
 
-    // 3. Estos métodos "On...Changed" los detecta el Toolkit automáticamente.
-    // Se ejecutan solos cada vez que cambian las variables de arriba.
     partial void OnTotalConsumoChanged(decimal value) => CalcularValores();
     partial void OnNumeroPersonasChanged(int value) => CalcularValores();
     partial void OnPorcentajePropinaChanged(int value) => CalcularValores();
 
-    // 4. Usamos [RelayCommand] para convertir métodos normales en Comandos
     [RelayCommand]
     private void SeleccionarPropina(string porcentaje)
     {
@@ -61,7 +54,6 @@ public partial class CalculadoraViewModel : ObservableObject
             NumeroPersonas--;
     }
 
-    // Método privado normal con la lógica de negocio
     private void CalcularValores()
     {
         if (NumeroPersonas <= 0) return;
